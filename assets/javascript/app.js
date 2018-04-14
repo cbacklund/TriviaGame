cf3b2df5e8eb0aca3f5f0f3084d04899a9ac26ce
Craig Backlund <craig.backlund@gmail.com>
2018-04-13 20:35:00 -0500

    Garbage fest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,9 +10,7 @@ function Trivia(question, a1, a2, a3, a4, correct, pic) {
 }
 
 //create variables to track answers submitted
-var correctA = 0;
-var incorrect = 0;
-var unanswered = 0;
+var correctA = incorrectA = unansweredA = 0;
 
 // creating the questions and array of the trivia game
 var q = [
@@ -29,23 +27,44 @@ var q = [
 ];
 
 console.log(q[0]);
+console.log(q[0].a1);
+
+$("#f").autofill(q);
 
 // create the start function
 function start() {
-
+    for (var j = 0; 0 < q.length; j++) {
+        var quest = $("<h2>");
+        var ans1 = ans2 = ans3 = ans4 = $("<button>");
+        quest.text(q[j].a0);
+        ans1.text(q[j].a1);
+        ans2.text(q[j].a2);
+        ans3.text(q[j].a3);
+        ans4.text(q[j].a4);
+        $("#question").text(quest);
+        $("#answer1").append(ans1);
+        console.log(ans1);
+        console.log(ans2);
+        console.log(ans3);
+        console.log(ans4);
+    } // for loop close out
 } //start function close out
+
+start();
 
 // create the reset function
 function reset() {
     correctA = incorrect = unanswered = 0;
 } //reset function close out
 
-// create the submit button to move to next question
-function picMove() {
+// create the finish function that will execute when the submit button is pushed
+function submit() {
+
    
 } //submit function close out
 
 function check(a, b) {
+    for (var i = 0; 0 < q.length; q++) {
     if (a == b) {
         correctA++;
         $("#correct").text(correctA);
@@ -53,4 +72,5 @@ function check(a, b) {
         incorrect++;
         $("#incorrect").text(incorrect);
     }
+} // for loop close out
 }// check close out
